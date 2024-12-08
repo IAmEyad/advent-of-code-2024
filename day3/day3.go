a286cad2ae8e0fd5bd64a34794fa3a1ce1ede1f9
Eyad Hasan <iameyad@protonmail.com>
2024-12-07 19:48:58 -0500

    Day 3 done

--- day3/day3.go
+++ day3/day3.go
@@ -4,6 +4,9 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"regexp"
+	"strconv"
+	"strings"
 )
 
 func main() {
@@ -11,6 +14,26 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Println(file)
+
+	// I said I wasn't going to use regex but here I am
+	r, _ := regexp.Compile(`(mul)+\(+[[:digit:]]+,[[:digit:]]+\)`)
+	n, _ := regexp.Compile(`[[:digit:]]+,[[:digit:]]+`)
+
+	//fmt.Println(r.FindAllString(string(file), -1))
+	filtered_file := r.FindAllString(string(file), -1)
+
+	result := 0
+	// maybe?
+	for _, i := range filtered_file {
+
+		clean_i := n.FindAllString(string(i), -1)
+		clean_i = strings.Split(clean_i[0], ",")
+
+		in_1, _ := strconv.Atoi(clean_i[0])
+		in_2, _ := strconv.Atoi(clean_i[1])
+		result += (in_1 * in_2)
+	}
+
+	fmt.Println(result)
 
 }
